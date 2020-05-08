5ee502f7cd804e467548d61eaf1d27e4d2bc5886
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-05-07 19:47:41 -0700

    completed cgi activity

--- cgi-bin/cgi_1.py
+++ cgi-bin/cgi_1.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import cgi
+import cgitb
 
+cgitb.enable()
+
+# 1/0 # introducing an error to use debug (cgitb)
 
 cgi.test()
--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,17 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+operands = form.getlist('operand')
+
+try:
+	# sum(): adds up the values
+	# map(int()): convert list of strings to integers
+	total = sum(map(int, operands))
+	body = "Your total is: {}".format(total)
+except (ValueError, TypeError):
+	body = "Unable to calculate a sum: please provide integer operands."
+
 print("Content-type: text/plain")
 print()
-print("Your job is to make this work")
+print(body)
